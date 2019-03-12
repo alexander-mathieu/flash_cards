4a24996b6eb3af85ffdf53f2c814231c016b4376
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-12 13:06:02 -0600

    Adds card class

---
+++ lib/card.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Card
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+
+  def question
+    @question
+  end
+
+  def answer
+    @answer
+  end
+
+  def category
+    @category
+  end
+end
+
+binding.pry
