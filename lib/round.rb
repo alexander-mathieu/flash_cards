536db1b4d993f81fa0833028ba52e151389efeb9
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-14 21:57:33 -0600

    Add new round feature and corresponding test

--- lib/round.rb
+++ lib/round.rb
@@ -11,21 +11,29 @@ class Round
     @turns = []
     @correct_count = 0
     @turns_taken = 0
+    @cards_by_category = []
+    @correct_guesses = []
+    @correct_by_category = []
   end
 
   def turns
     @turns
   end
 
+  def deck_index
+    @turns.count
+  end
+
   def current_card
-    deck.cards.first
+    deck.cards[deck_index]
+    #deck.cards.first
   end
 
   def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
     @turns << new_turn
     if new_turn.correct? == true
-      @correct_count +=1
+      @correct_count += 1
     end
     @turns_taken += 1
     new_turn
@@ -39,19 +47,33 @@ class Round
     @correct_count.fdiv(@turns_taken) * 100
   end
 
-  def percent_correct_by_category(card_category)
-
+  def number_correct_by_category(card_category)
+    @turns.each do |card|
+      if card.correct?
+        @correct_guesses << card
+      end
+    end
+    @correct_guesses.each do |card|
+      if card.card.category == card_category
+        @correct_by_category << card
+      end
+    end
+    @correct_by_category.count
   end
+
+
+  # def percentage_correct_by_category(card_category)
+  #   @turns.each do |card|
+  #     if card.correct?
+  #       @correct_guesses << card
+  #     end
+  #     @correct_guesses
+  #   # @turns.each do |card|
+  #   #   if card.card.category == card_category
+  #   #     @cards_by_category << card
+  #   #   end
+  #   end
+  # end
 end
 
-# @turns.each do |card|
-#   if card.correct?
-#     @correct_guesses << card
-#   end
-# end
-# @correct_guesses.each do |card|
-#   if card.card.category == card_category
-#     @correct_by_category << card
-#   end
-# end
-# @correct_by_category.count
+# @correct_by_category.count.fdiv(@cards_by_category.count) * 100
--- test/round_test.rb
+++ test/round_test.rb
@@ -20,7 +20,7 @@ class RoundTest < MiniTest::Test
     assert_instance_of Round, round
   end
 
-  def test_deck_object_exists
+  def test_deck_object_exists_within_round
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -87,7 +87,7 @@ class RoundTest < MiniTest::Test
 
     round = Round.new(deck)
 
-    round.take_turn("Juneau")
+    new_turn = round.take_turn("Juneau")
 
     assert_equal 1, round.turns.length
   end
@@ -113,7 +113,7 @@ class RoundTest < MiniTest::Test
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -129,7 +129,7 @@ class RoundTest < MiniTest::Test
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -145,7 +145,7 @@ class RoundTest < MiniTest::Test
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -153,7 +153,7 @@ class RoundTest < MiniTest::Test
 
     new_turn = round.take_turn("Juneau")
 
-    round.take_turn("Venus")
+    new_turn = round.take_turn("Venus")
 
     assert_equal 2, round.turns.count
   end
@@ -163,7 +163,7 @@ class RoundTest < MiniTest::Test
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -173,7 +173,7 @@ class RoundTest < MiniTest::Test
 
     assert_equal "Correct!", round.turns.last.feedback
 
-    round.take_turn("Venus")
+    new_turn = round.take_turn("Venus")
 
     assert_equal "Incorrect.", round.turns.last.feedback
   end
@@ -183,7 +183,7 @@ class RoundTest < MiniTest::Test
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -191,17 +191,17 @@ class RoundTest < MiniTest::Test
 
     new_turn = round.take_turn("Juneau")
 
-    round.take_turn("Venus")
+    new_turn = round.take_turn("Venus")
 
    assert_equal 50.0, round.percent_correct
   end
 
-  def test_round_shows_percent_correct_by_category
+  def test_round_shows_number_correct_by_category
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°     clockwise from due north?", "North north west", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1, card_2, card_3])
 
@@ -209,8 +209,33 @@ class RoundTest < MiniTest::Test
 
     new_turn = round.take_turn("Juneau")
 
-    round.take_turn("Venus")
+    assert_equal 1, round.number_correct_by_category(:Geography)
 
-    assert_equal 100.0, round.percent_correct_by_category(:STEM)
+    new_turn = round.take_turn("Venus")
+
+    assert_equal 0, round.number_correct_by_category(:STEM)
   end
+
+#   def test_round_shows_percentage_correct_by_category
+#     skip
+#     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#
+#     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+#
+#     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+#
+#     deck = Deck.new([card_1, card_2, card_3])
+#
+#     round = Round.new(deck)
+#
+#     new_turn = round.take_turn("Juneau")
+#
+#     new_turn = round.take_turn("Mars")
+#
+#     #round.take_turn("North north west")
+#
+#     assert_equal 100.0, round.percentage_correct_by_category(:Geography)
+#   end
+#
+#     #assert_equal 50.0, round.percentage_correct_by_category(:STEM)
 end
